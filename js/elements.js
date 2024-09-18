010f5415ed01edabdf22cef875024da1950c342c
Night Kings Studio <ben1amyn1@gmail.com>
2024-09-18 16:27:41 +0200

    2.0 Update 2.3

--- js/elements.js
+++ js/elements.js
@@ -18,3 +18,21 @@ document.addEventListener('DOMContentLoaded', function() {
     loadHTML('inc/header.html', 'header-placeholder');
     loadHTML('inc/footer.html', 'footer-placeholder');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the buttons by their class names
+    const steamButton = document.querySelector('.sign-in-button');
+    const loginButton = document.querySelector('.sign-in-button1');
+
+    // Add click event listener to the Steam button
+    steamButton.addEventListener('click', function() {
+        // Redirect to Steam login page (or perform other actions)
+        window.location.href = '/NightKings/404';
+    });
+
+    // Add click event listener to the Login button
+    loginButton.addEventListener('click', function() {
+        // Redirect to login page or another URL
+        window.location.href = 'id/log-in'; 
+    });
+});
